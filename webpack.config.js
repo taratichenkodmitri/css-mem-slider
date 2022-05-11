8eac6f399217884603399247d2044b8bb1199d57
taratichenkodmitri <tarataichenkodmitri@gmail.com>
2022-05-11 14:38:49 +0600

    feat: add webpack (Wed, May 11, 2022 14:38:49 PM)

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = process.env.NODE_ENV === 'production';
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    };
+
+    if(isProd) {
+        config.minimizer = [
+            new CssMinimizerPlugin(),
+            new TerserPlugin()
+        ];
+    }
+
+    return config;
+}
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`;
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    mode: 'development',
+    entry: {
+        main: './main.js'
+    },
+    output: {
+        filename: filename('js'),
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: optimization(),
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html'
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            }
+        ]
+    },
+    devServer: {
+        port: 4200,
+        hot: isDev
+    }
+}
